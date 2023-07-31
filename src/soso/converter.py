2c726042ceae2c01d01a9e3fb21b5890bdc82f94
Colin Smith <colin.smith@wisc.edu>
2023-07-31 10:05:39 -0700

    Stub out methods and tests
    Stub out methods for the interface, EML, and ISO-19115 modules to
    further verify suitability of the strategy design pattern. Create
    one method for each SOSO property, each of which represent a single
    and reasonably small concept.
    
    Stub out tests to veryify the generalized testing interface and
    approach.

--- src/soso/converter.py
+++ src/soso/converter.py
@@ -16,5 +16,59 @@ def convert(strategy):
         raise ValueError("Invalid choice!")
 
     # Build the graph
-    res = {"name": strategy.set_name(), "description": strategy.set_description()}
+    res = {
+        "name": strategy.get_name(),
+        "description": strategy.get_description(),
+        "url": strategy.get_url(),
+        "sameAs": strategy.get_same_as(),
+        "version": strategy.get_version(),
+        "isAccessibleForFree": strategy.get_is_accessible_for_free(),
+        "keywords": strategy.get_keywords(),
+        "identifier": strategy.get_identifier(),
+        "citation": strategy.get_citation(),
+        "variableMeasured": strategy.get_citation(),
+        "includedInDataCatalog": strategy.get_included_in_data_catalog(),
+        "subjectOf": strategy.get_subject_of(),
+        "distribution": strategy.get_distribution(),
+        "dateCreated": strategy.get_date_created(),
+        "dateModified": strategy.get_date_modified(),
+        "datePublished": strategy.get_date_published(),
+        "expires": strategy.get_expires(),
+        "temporalCoverage": strategy.get_temporal_coverage(),
+        "spatialCoverage": strategy.get_spatial_coverage(),
+        "creator": strategy.get_creator(),
+        "contributor": strategy.get_creator(),
+        "provider": strategy.get_provider(),
+        "publisher": strategy.get_publisher(),
+        "funding": strategy.get_funding(),
+        "license": strategy.get_license(),
+        "wasRevisionOf": strategy.get_was_revision_of(),
+        "wasDerivedFrom": strategy.get_was_derived_from(),
+        "isBasedOn": strategy.get_is_based_on(),
+        "wasGeneratedBy": strategy.get_was_generated_by(),
+        "checksum": strategy.get_checksum()
+    }
     return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tests/conftest.py
+++ tests/conftest.py
@@ -18,12 +18,87 @@ def strategy_instance(request):
 
 
 @pytest.fixture
-def interface_methods():
-    """Return the names of strategy methods."""
-    return ["set_name", "set_description"]
+def soso_properties():
+    """Return the names of SOSO properties."""
+    return [
+        "name",
+        "description",
+        "url",
+        "sameAs",
+        "version",
+        "isAccessibleForFree",
+        "keywords",
+        "identifier",
+        "citation",
+        "variableMeasured",
+        "includedInDataCatalog",
+        "subjectOf",
+        "distribution",
+        "dateCreated",
+        "dateModified",
+        "datePublished",
+        "expires",
+        "temporalCoverage",
+        "spatialCoverage",
+        "creator",
+        "contributor",
+        "provider",
+        "publisher",
+        "funding",
+        "license",
+        "wasRevisionOf",
+        "wasDerivedFrom",
+        "isBasedOn",
+        "wasGeneratedBy",
+        "checksum"
+    ]
 
 
 @pytest.fixture
-def soso_properties():
-    """Return the names of SOSO properties."""
-    return ["name", "description"]
+def interface_methods():
+    """Return the names of strategy methods."""
+    res = [
+        "get_name",
+        "get_description",
+        "get_url",
+        "get_same_as",
+        "get_version",
+        "get_is_accessible_for_free",
+        "get_keywords",
+        "get_identifier",
+        "get_citation",
+        "get_variable_measured",
+        "get_included_in_data_catalog",
+        "get_subject_of",
+        "get_distribution",
+        "get_date_created",
+        "get_date_modified",
+        "get_date_published",
+        "get_expires",
+        "get_temporal_coverage",
+        "get_spatial_coverage",
+        "get_creator",
+        "get_contributor",
+        "get_provider",
+        "get_publisher",
+        "get_funding",
+        "get_license",
+        "get_was_revision_of",
+        "get_was_derived_from",
+        "get_is_based_on",
+        "get_was_generated_by",
+        "get_checksum"
+    ]
+    return res
+
+
+
+
+
+
+
+
+
+
+
+
