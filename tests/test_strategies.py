e49a719dd68a7d7de32053728795c8aa1ac39e1a
Colin Smith <colin.smith@wisc.edu>
2024-03-21 15:31:22 -0700

    Save WIP

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -41,37 +41,34 @@ def test_get_description_returns_expected_type(strategy_instance):
     assert is_property_type(res, ["schema:Text"])
 
 
+@pytest.mark.skipif(strategy_instance="EML", reason="Property not in schema")
 def test_get_url_returns_expected_type(strategy_instance):
     """Test that the get_url method returns the expected type."""
-    # url doesn't map to EML so use kwargs
-    strategy_instance.kwargs = {"url": "https://example.com"}
     res = strategy_instance.get_url()
     assert is_not_null(res)
     assert is_property_type(res, ["schema:URL"])
 
 
+@pytest.mark.skipif(strategy_instance="EML", reason="Property not in schema")
 def test_get_same_as_returns_expected_type(strategy_instance):
     """Test that the get_same_as method returns the expected type."""
-    strategy_instance.kwargs = {"sameAs": "https://example.com"}
     res = strategy_instance.get_same_as()
     assert is_not_null(res)
     assert is_property_type(res, ["schema:URL"])
 
 
+@pytest.mark.skipif(strategy_instance="EML", reason="Property not in schema")
 def test_get_version_returns_expected_type(strategy_instance):
     """Test that the get_version method returns the expected type."""
-    version_values = ["1.0", 1.0]
-    for value in version_values:
-        strategy_instance.kwargs = {"version": value}
-        res = strategy_instance.get_version()
-        assert is_not_null(res)
-        assert is_property_type(res, ["schema:Text", "schema:Number"])
+    res = strategy_instance.get_version()
+    assert is_not_null(res)
+    assert is_property_type(res, ["schema:Text", "schema:Number"])
 
 
+@pytest.mark.skipif(strategy_instance="EML", reason="Property not in schema")
 def test_get_is_accessible_for_free_returns_expected_type(strategy_instance):
     """Test that the get_is_accessible_for_free method returns the expected
     type."""
-    strategy_instance.kwargs = {"isAccessibleForFree": True}
     res = strategy_instance.get_is_accessible_for_free()
     assert is_not_null(res)
     assert is_property_type(res, ["schema:Boolean"])
@@ -109,37 +106,18 @@ def test_get_variable_measured_returns_expected_type(strategy_instance):
     assert is_property_type(res, ["schema:PropertyValue"])
 
 
+@pytest.mark.skipif(strategy_instance="EML", reason="Property not in schema")
 def test_get_included_in_data_catalog_returns_expected_type(strategy_instance):
     """Test that the get_included_in_data_catalog method returns the expected
     type."""
-    strategy_instance.kwargs = {
-        "includedInDataCatalog": {
-            "@type": "DataCatalog",
-            "name": "Biological Data",
-            "description": "A catalog of biological data.",
-            "url": "https://www.sample-data-repository.org/collection/biological-data",
-        }
-    }
     res = strategy_instance.get_included_in_data_catalog()
     assert is_not_null(res)
     assert is_property_type(res, ["schema:DataCatalog"])
 
 
+@pytest.mark.skipif(strategy_instance="EML", reason="Property not in schema")
 def test_get_subject_of_returns_expected_type(strategy_instance):
     """Test that the get_subject_of method returns the expected type."""
-    strategy_instance.kwargs = {
-        "subjectOf": {
-            "@type": "DataDownload",
-            "name": "Metadata for dataset",
-            "description": "Metadata describing the dataset",
-            "encodingFormat": [
-                "application/xml",
-                "https://metadata.schema/version-1.0.0",
-            ],
-            "contentURL": "https://example.com/metadata/eml-metadata.xml",
-            "dateModified": "2019-06-12T14:44:15Z",
-        }
-    }
     res = strategy_instance.get_subject_of()
     assert is_not_null(res)
     assert is_property_type(res, ["schema:DataDownload"])
@@ -152,69 +130,9 @@ def test_get_distribution_returns_expected_type(strategy_instance):
     assert is_property_type(res, ["schema:DataDownload"])
 
 
+@pytest.mark.skipif(strategy_instance="EML", reason="Property not in schema")
 def test_get_potential_action_returns_expected_type(strategy_instance):
     """Test that the get_potential_action method returns the expected type."""
-    strategy_instance.kwargs = {
-        "potentialAction": {
-            "@type": "SearchAction",
-            "target": {
-                "@type": "EntryPoint",
-                "contentType": ["application/x-netcdf", "text/tab-separated-values"],
-                "urlTemplate": "https://www.sample-data-repository.org/"
-                "dataset/1234/download?format={format}&"
-                "startDateTime={start}&endDateTime={end}&bounds"
-                "={bbox}",
-                "description": "Download dataset 1234 based on the requested "
-                "format, start/end dates and bounding box",
-                "httpMethod": ["GET", "POST"],
-            },
-            "query-input": [
-                {
-                    "@type": "PropertyValueSpecification",
-                    "valueName": "format",
-                    "description": "The desired format requested either "
-                    "'application/x-netcdf' or 'text/tab-"
-                    "separated-values'",
-                    "valueRequired": True,
-                    "defaultValue": "application/x-netcdf",
-                    "valuePattern": r"(application\/x-netcdf|text\/tab-"
-                    "separated-values)",
-                },
-                {
-                    "@type": "PropertyValueSpecification",
-                    "valueName": "start",
-                    "description": "A UTC ISO DateTime",
-                    "valueRequired": False,
-                    "valuePattern": "(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0"
-                    "[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|"
-                    "[01][0-9]):([0-5][0-9]):([0-5][0-9])"
-                    "(.[0-9]+)?(Z)?",
-                },
-                {
-                    "@type": "PropertyValueSpecification",
-                    "valueName": "end",
-                    "description": "A UTC ISO DateTime",
-                    "valueRequired": False,
-                    "valuePattern": "(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0"
-                    "[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|"
-                    "[01][0-9]):([0-5][0-9]):([0-5][0-9])"
-                    "(.[0-9]+)?(Z)?",
-                },
-                {
-                    "@type": "PropertyValueSpecification",
-                    "valueName": "bbox",
-                    "description": "Two points in decimal degrees that create "
-                    "a bounding box fomatted at 'lon,lat' of "
-                    "the lower-left corner and 'lon,lat' of "
-                    "the upper-right",
-                    "valueRequired": False,
-                    "valuePattern": "(-?[0-9]+(.[0-9]+)?),[ ]*(-?[0-9]+(.[0-9]"
-                    "+)?)[ ]*(-?[0-9]+(.[0-9]+)?),[ ]*(-?[0-9]"
-                    "+(.[0-9]+)?)",
-                },
-            ],
-        }
-    }
     res = strategy_instance.get_potential_action()
     assert is_not_null(res)
     assert is_property_type(res, ["schema:SearchAction"])
@@ -242,9 +160,9 @@ def test_get_date_published_returns_expected_type(strategy_instance):
     assert is_property_type(res, ["schema:Date", "schema:DateTime"])
 
 
+@pytest.mark.skipif(strategy_instance="EML", reason="Property not in schema")
 def test_get_expires_returns_expected_type(strategy_instance):
     """Test that the get_expires method returns the expected type."""
-    strategy_instance.kwargs = {"expires": "2019-06-12T14:44:15Z"}
     res = strategy_instance.get_expires()
     assert is_not_null(res)
     assert is_property_type(res, ["schema:Date", "schema:DateTime"])
@@ -294,21 +212,17 @@ def test_get_contributor_returns_expected_type(strategy_instance):
     )
 
 
+@pytest.mark.skipif(strategy_instance="EML", reason="Property not in schema")
 def test_get_provider_returns_expected_type(strategy_instance):
     """Test that the get_provider method returns the expected type."""
-    strategy_instance.kwargs = {
-        "provider": {"@id": "https://www.sample-data-repository.org"}
-    }
     res = strategy_instance.get_provider()
     assert is_not_null(res)
     assert is_property_type(res, ["schema:Organization", "@id"])
 
 
+@pytest.mark.skipif(strategy_instance="EML", reason="Property not in schema")
 def test_get_publisher_returns_expected_type(strategy_instance):
     """Test that the get_publisher method returns the expected type."""
-    strategy_instance.kwargs = {
-        "publisher": {"@id": "https://www.sample-data-repository.org"}
-    }
     res = strategy_instance.get_publisher()
     assert is_not_null(res)
     assert is_property_type(res, ["schema:Organization", "@id"])
@@ -352,17 +266,9 @@ def test_get_is_based_on_returns_expected_type(strategy_instance):
     assert is_property_type(res, ["@id"])
 
 
+@pytest.mark.skipif(strategy_instance="EML", reason="Property not in schema")
 def test_get_was_generated_by_returns_expected_type(strategy_instance):
     """Test that the get_was_generated_by method returns the expected type."""
-    strategy_instance.kwargs = {
-        "wasGeneratedBy": {
-            "@id": "https://example.org/executions/execution-42",
-            "@type": "provone:Execution",
-            "prov:hadPlan": "https://somerepository.org/datasets/10.xxxx/"
-            "Dataset-2.v2/process-script.R",
-            "prov:used": {"@id": "https://doi.org/10.xxxx/Dataset-1"},
-        }
-    }
     res = strategy_instance.get_was_generated_by()
     assert is_not_null(res)
     assert is_property_type(res, ["provone:Execution"])
