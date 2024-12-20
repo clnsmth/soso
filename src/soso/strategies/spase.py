018a2affa3bc2e12c9d2e4a41f91887ac70eb65b
Zach Boquet <zcboquet@gmail.com>
2024-12-20 17:10:31 -0600

    feat: adding mapping implementation for citation, identifier, and creators (clnsmth#213)

--- src/soso/strategies/spase.py
+++ src/soso/strategies/spase.py
@@ -31,7 +31,7 @@ class SPASE(StrategyInterface):
         <https://github.com/ESIPFed/science-on-schema.org/blob/master/guides/Dataset.md>`_.
 
         Below are unmappable properties for this strategy:
-            - [Add unmappable properties here]
+            - [includedInDataCatalog]
     """
 
     def __init__(self, file: str, **kwargs: dict):
@@ -44,6 +44,7 @@ class SPASE(StrategyInterface):
         self.schema_version = get_schema_version(self.metadata)
         self.namespaces = {"spase": "http://www.spase-group.org/data/schema"}
         self.kwargs = kwargs
+        self.root = self.metadata.getroot()
 
     def get_id(self) -> str:
         # Mapping: schema:identifier = spase:ResourceID
@@ -88,20 +89,90 @@ class SPASE(StrategyInterface):
         version = None
         return delete_null_values(version)
 
-    def get_is_accessible_for_free(self) -> None:
+    # commented out partial code that was put on hold due to licenses being added to SPASE soon
+    def get_is_accessible_for_free(self) -> bool:
+        """schema:description: spase:AccessInformation/AccessRights"""
         is_accessible_for_free = None
+        # local vars needed
+        #access = ""
+
+        # iterate thru to find AccessInfo
+        #for child in self.root[1]:
+        #    if access == "Open":
+        #        break
+        #    if child.tag.endswith("AccessInformation"):
+        #        targetChild = child
+                # iterate thru to find AccessRights
+        #        for child in targetChild:
+        #            if child.tag.endswith("AccessRights"):
+        #                access = child.text 
+        #if access == "Open":
+        #    is_accessible_for_free = True
+        #else:
+        #    is_accessible_for_free = False
         return delete_null_values(is_accessible_for_free)
 
     def get_keywords(self) -> None:
         keywords = None
         return delete_null_values(keywords)
 
-    def get_identifier(self) -> None:
-        identifier = None
+    def get_identifier(self) -> tuple:
+        # Mapping: schema:identifier = spase:ResourceHeader/spase:DOI (or https://hpde.io landing page, if no DOI)
+        url = self.get_url()
+        # if SPASE record has a DOI
+        if "doi" in url:
+            temp = url.split("/")
+            value = "doi:" + "/".join(temp[3:])
+            identifier = {"@id": f"{url}",
+                        "@type" : "PropertyValue",
+                        "propertyID": "https://registry.identifiers.org/registry/doi",
+                        "value": f"{value}",
+                        "url": f"{url}"}
+        # if SPASE record only has landing page instead
+        else:
+            identifier = url
         return identifier
 
-    def get_citation(self) -> None:
-        citation = None
+    def get_citation(self) -> str:
+        # Mapping: schema:citation = spase:ResourceHeader/spase:PublicationInfo/spase:Authors
+        # AND spase:ResourceHeader/spase:PublicationInfo/spase:PublicationDate
+        # AND spase:ResourceHeader/spase:PublicationInfo/spase:PublishedBy
+        # AND spase:ResourceHeader/spase:PublicationInfo/spase:Title
+        # AND spase:ResourceHeader/spase:DOI
+
+        # local vars needed
+        author = ""
+        pubDate = ""
+        pub = ""
+        dataset = ""
+
+        # iterate thru to find ResourceHeader
+        for child in self.root[1]:
+            if child.tag.endswith("ResourceHeader"):
+                targetChild = child
+                # iterate thru to find PublicationInfo
+                for child in targetChild:
+                    if child.tag.endswith("PublicationInfo"):
+                        PI_child = child
+                        for child in PI_child:
+                            # collect preferred author
+                            if child.tag.endswith("Authors"):
+                                author = child.text
+                                author = author.replace(";", ",")
+                            elif child.tag.endswith("PublicationDate"):
+                                pubDate = child.text
+                                pubDate = pubDate[:4]
+                            # collect preferred publisher
+                            elif child.tag.endswith("PublishedBy"):
+                                pub = child.text
+                            # collect preferred dataset
+                            elif child.tag.endswith("Title"):
+                                dataset = child.text
+        DOI = self.get_url()
+        if dataset:
+            citation = f"{author} ({pubDate}). {dataset}. {pub}. {DOI}"
+        else:
+            citation = f"{author} ({pubDate}). {pub}. {DOI}"
         return delete_null_values(citation)
 
     def get_variable_measured(self) -> None:
@@ -163,8 +234,49 @@ class SPASE(StrategyInterface):
             )
         return delete_null_values(spatial_coverage)
 
-    def get_creator(self) -> None:
-        creator = None
+    def get_creator(self) -> list:
+        # Mapping: schema:creator = spase:ResourceHeader/spase:PublicationInfo/spase:Authors 
+        # OR schema:creator = spase:ResourceHeader/spase:Contact/spase:PersonID
+        
+        author, authorRole = get_authors(self.metadata)
+        authorStr = str(author)
+        authorStr = authorStr.replace("[", "").replace("]","")
+        creator = []
+        print(authorStr)
+        if ";" in authorStr:
+            author = authorStr.split(";")
+        else:
+            # unfinished: need to fix number of times loop runs since this runs up to num of chars in string
+            path, sep, author = authorStr.partition("Person/")
+        # iterate over each person in author string
+        for person in author:
+            # keep track of position so roles will match
+            print(len(author))
+            index = author.index(person)
+            print(index)
+            # get rid of extra quotations
+            person = person.replace("'","")
+            # if all creators were found in PublicationInfo/Authors
+            if authorRole == ["Author"]:
+                familyName, sep, givenName = person.partition(",")
+                creator.append({"@type": "Role", 
+                                "roleName": f"{authorRole[0]}",
+                                "creator": {"@type": "Person",
+                                            "name": f"{person}",
+                                            "givenName": f"{givenName}",
+                                            "familyName": f"{familyName}"}
+                                            })
+            else:
+                givenName, sep, familyName = person.partition(".")
+                initial, sep, familyName = familyName.partition(".")
+                givenName = givenName + "." + initial
+                creator.append({"@type": "Role", 
+                                "roleName": f"{authorRole[index]}",
+                                "creator": {"@type": "Person",
+                                            "name": f"{person}",
+                                            "givenName": f"{givenName}",
+                                            "familyName": f"{familyName}"}
+                                            })
         return delete_null_values(creator)
 
     def get_contributor(self) -> None:
@@ -217,3 +329,67 @@ def get_schema_version(metadata: etree.ElementTree) -> str:
         "{http://www.spase-group.org/data/schema}Version"
     )
     return schema_version
+
+def get_authors(metadata: etree.ElementTree) -> tuple:
+    """
+    :param metadata:    The SPASE metadata object as an XML tree.
+    
+    :returns: The highest priority authors found within the SPASE record as a list
+                as well as a list of their roles, respectively.
+    """
+    # local vars needed
+    author = []
+    authorRole = []
+    PI_child = None
+    priority = False
+    root = metadata.getroot()
+    # holds role values that are not considered for author var
+    UnapprovedAuthors = ["MetadataContact", "ArchiveSpecialist",
+                        "HostContact", "Publisher", "User"]
+
+    # iterate thru to find ResourceHeader
+    for child in root[1]:
+        if child.tag.endswith("ResourceHeader"):
+            targetChild = child
+            # iterate thru to find PublicationInfo
+            for child in targetChild:
+                if child.tag.endswith("PublicationInfo"):
+                    PI_child = child
+                elif child.tag.endswith("Contact"):
+                    C_Child = child
+                    # iterate thru Contact to find PersonID and Role
+                    for child in C_Child:
+                        # find PersonID
+                        if child.tag.endswith("PersonID"):
+                            # store PersonID
+                            PersonID = child.text
+                        # find Role
+                        elif child.tag.endswith("Role"):
+                            # backup author
+                            if ("PrincipalInvestigator" or "PI") in child.text:
+                                # if a lesser priority author found
+                                #     first, overwrite author lists
+                                if not priority and author:
+                                    author = [PersonID]
+                                    authorRole = [child.text]
+                                else:
+                                    author.append(PersonID)
+                                    authorRole.append(child.text)
+                                # mark that highest priority backup author was found
+                                priority = True
+    if PI_child is not None:
+        for child in PI_child:
+            # collect preferred author
+            if child.tag.endswith("Authors"):
+                author = [child.text]
+                authorRole = ["Author"]
+    return author, authorRole
+
+#testSpase = SPASE("C:/Users/zboqu/NASA Internship/soso-spase/src/soso/data/spase.xml")
+testSpase = SPASE("C:/Users/zboqu/NASA Internship/NASA/NumericalData/Cassini/MAG/PT60S.xml")
+#print(testSpase.get_is_accessible_for_free())
+#print(testSpase.get_citation())
+#print(testSpase.get_identifier())
+result = testSpase.get_creator()
+for each in result:
+    print(each)
