47cae46d5a84ba55f2c639b70cc969e3b750338e
Zach Boquet <133173973+Kurokio@users.noreply.github.com>
2026-09-11 19:17:32 +0000

    feat: Add MIME Type Mapping (#354)
    This SPASE strategy update introduces a mapping between the
    potentialActions:target:contentTypes and their corresponding MIME types.
    This enhancement ensures that existing FAIR assessment tools can
    properly determine that the "Data is available in a file format
    recommended by the target research community", thereby further improving
    the FAIRness of SPASE records. Resolves
    https://github.com/pangaea-data-publisher/fuji/issues/595
    
    ---------
    
    Co-authored-by: Rebecca Ringuette <49281118+rebeccaringuette@users.noreply.github.com>

--- src/soso/strategies/spase/spase.py
+++ src/soso/strategies/spase/spase.py
@@ -480,6 +480,63 @@ class SPASE(StrategyInterface):
             )
             multiple = False
 
+            # map encoding to equivalent MIME Types
+            match encoding:
+                case "AVI":
+                    encoding = "application/x-troff-msvideo/"
+                case "Binary":
+                    encoding = "application/vnd.debian.binary-package/"
+                case "CDF":
+                    encoding = "application/x-cdf/"
+                case "CEF" | "CEF1" | "CEF2" | "RINEX2" | "RINEX3" | "Text.ASCII":
+                    encoding = "text/asciidoc/"
+                case "CSV":
+                    encoding = "application/csv/"
+                case "Excel":
+                    encoding = "application/msexcel/"
+                case "FITS":
+                    encoding = "application/fits/"
+                case "GIF":
+                    encoding = "image/gif/"
+                case "HDF":
+                    encoding = "application/x-hdf/"
+                case "HDF4":
+                    encoding = "application/x-hdf4/"
+                case "HDF5":
+                    encoding = "application/x-hdf5/"
+                case "HTML":
+                    encoding = "text/html/"
+                case "IDL":
+                    encoding = "text/x-idl/"
+                case "JPEG":
+                    encoding = "image/jpeg/"
+                case "JSON" | "TFCat":
+                    encoding = "application/json/"
+                case "MATLAB_4" | "MATLAB_6" | "MATLAB_7":
+                    encoding = "application/matlab-mat"
+                case "MPEG":
+                    encoding = "video/mpeg/"
+                case "NetCDF":
+                    encoding = "application/x-netcdf/"
+                case "PDF":
+                    encoding = "application/pdf/"
+                case "PDS3" | "PDS4" | "XML":
+                    encoding = "application/xml/"
+                case "PNG":
+                    encoding = "image/png/"
+                case "Postscript":
+                    encoding = "application/postscript/"
+                case "QuickTime":
+                    encoding = "video/quicktime/"
+                case "TIFF":
+                    encoding = "image/tiff/"
+                case "VOTable":
+                    encoding = "application/x-votable+xml/"
+                case _:
+                    print(
+                        "Content type cannot be mapped to MIME type. Leaving as-is."
+                    )  # This underscore is the "catch-all" default
+
             # most basic format for a potentialAction item
             entry = {
                 "@type": "SearchAction",
@@ -1173,11 +1230,11 @@ class SPASE(StrategyInterface):
 
         # commenting out observatories because of the email with Baptiste and Donny
         instruments = get_instrument(self.metadata, self.file)
-        # only uncomment if trying to generate snapshot spase.json
+        # observatories = get_observatory(self.metadata, self.file)
+        # only uncomment instrument lines below if trying to generate snapshot spase.json
         # instruments = get_instrument(
         #    self.metadata, self.file, **{"testing": "soso-spase/tests/data/spase/"}
         #    )
-        # observatories = get_observatory(self.metadata, self.file)
         was_generated_by = []
 
         # if observatories:
